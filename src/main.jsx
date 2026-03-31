0b534e02a9265515110b78483c5d16ba3adf11ee
Prabhat <prabhatdwivedi30606@gmail.com>
2026-03-31 22:54:52 +0530

    Add fontsource for Outfit and Inter fonts

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,8 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import "leaflet/dist/leaflet.css";
 import App from "./App.jsx";
+import "@fontsource/outfit";
+import "@fontsource/inter";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
