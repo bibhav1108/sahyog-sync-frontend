4eb24df660d674a03b1b1aba2a1963988c8cbe32
Prabhat <prabhatdwivedi30606@gmail.com>
2026-04-14 19:11:53 +0530

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const BACKEND_BASE_URL = "http://localhost:8005";
+export const BACKEND_BASE_URL = import.meta.env.VITE_BACKEND_URL || "http://localhost:8005";
 const API = axios.create({
   baseURL: `${BACKEND_BASE_URL}/api/v1/`,
 });
